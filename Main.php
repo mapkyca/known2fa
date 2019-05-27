7f08d93761738321a196eebd5f8d2e4216329038
Marcus Povey <marcus@marcus-povey.co.uk>
2019-05-27 11:09:04 +0100

    Updating 2fa plugin for new api, and installer

---
+++ Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace IdnoPlugins\TwoFactorAuth {
+
+    class Main extends \Idno\Common\Plugin {
+
+	function registerPages() {
+
+	    // Register an account menu
+	    \Idno\Core\Idno::site()->template()->extendTemplate('account/menu/items', 'twofactorauth/menu');
+
+
+	    // Register the callback URL
+	    \Idno\Core\Idno::site()->routes()->addRoute('account/twofactorauth', '\IdnoPlugins\TwoFactorAuth\Pages\Settings');
+
+	    // Replace login
+	    \Idno\Core\Idno::site()->routes()->addRoute('/session/login', '\IdnoPlugins\TwoFactorAuth\Pages\Login', true);
+
+	    // QR Code
+	    \Idno\Core\Idno::site()->routes()->addRoute('/twofactorauth/qr\.png', '\IdnoPlugins\TwoFactorAuth\Pages\QR', true);
+	}
+
+    }
+
+}
